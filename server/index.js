026d406a027c1ee87fe0fbeb5bb332b438aeafe9
Ravi Bharti <118691241+ravikumar9519@users.noreply.github.com>
2025-05-23 17:38:11 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001","https://teamTaskNova.netlify.app"],
+    origin: ["http://localhost:3000", "http://localhost:3001","https://task-nova-ravi.netlify.app"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
